4285c9cbafbe5b7e5789d4640922e5538f2f11db
Tonya Berg <129787007+tberg02@users.noreply.github.com>
2023-04-12 10:43:44 -0500

    Update ProjectClass.java

--- project/ProjectClass.java
+++ project/ProjectClass.java
@@ -9,5 +9,47 @@ package com.mycompany.project;
  * @author tonya
  */
 public class ProjectClass {
-    
+    public String fetchResponseFromAPI() throws IOException {
+        try {
+            // create URL object for the API endpoint
+            URL url = new URL("https://ebay-sold-items-api.herokuapp.com/findCompletedItems");
+
+            // create a connection object for the URL
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            // set the request method and headers
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json");
+
+            // create the request body
+            String requestBody = "{\"keywords\":\"iPhone\",\"excluded_keywords\":\"locked cracked case box read LCD face\",\"max_search_results\":\"240\",\"category_id\":\"9355\",\"remove_outliers\":true,\"site_id\":\"0\",\"aspects\":[{\"name\":\"Model\",\"value\":\"Apple iPhone X\"},{\"name\":\"LH_ItemCondition\",\"value\":\"3000\"},{\"name\":\"Network\",\"value\":\"Unlocked\"},{\"name\":\"Storage Capacity\",\"value\":\"256 GB\"}]}";
+
+            // send the request body
+            con.setDoOutput(true);
+            OutputStream os = con.getOutputStream();
+            os.write(requestBody.getBytes());
+            os.flush();
+            os.close();
+
+            // read the response from the API
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // return the response
+            return response.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public String fetchAverageSellingPrice() throws IOException {
+        return fetchResponseFromAPI();
+    }
 }
